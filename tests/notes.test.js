9c3033043a1572e78250e26a10157a4ed362a965
Payton Lower <paytown@me.com>
2019-06-11 10:03:18 -0700

    GET method

--- tests/notes.test.js
+++ tests/notes.test.js
@@ -4,6 +4,13 @@ const mongoose = require('mongoose');
 const connect = require('../lib/utils/connect');
 const app = require('../lib/app');
 
+const createNote = note => {
+  return request(app)
+    .post('/api/v1/notes')
+    .send(note)
+    .then(res => res.body);
+};
+
 describe('Notes Routes', () => {
   beforeAll(() => {
     return connect();
@@ -28,6 +35,20 @@ describe('Notes Routes', () => {
           title: 'Title',
           body: 'Body'
         });
+      });
+  });
+
+  it('GET method', async() => {
+    const notes = await Promise.all([
+      createNote({ title: '1', body: 'body' }),
+      createNote({ title: '2', body: 'body' }),
+      createNote({ title: '3', body: 'body' }),
+    ]);
+
+    return request(app)
+      .get('/api/v1/notes')
+      .then(res => {
+        expect(res.body).toEqual(notes);
       });
   });
 });
